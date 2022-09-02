c6d1326c2094f7fe12051eed19f0787f5aa5c3e1
marx <wsjzs@hotmail.com>
2022-09-03 00:55:32 +0800

    feat: about

--- site/pages/about.vue
+++ site/pages/about.vue
@@ -5,34 +5,41 @@
         <div class="widget-header">关于</div>
         <div class="widget-content content">
           <h2>简介</h2>
+          <p>小润书&nbsp;初心是建立一个润友交流与学习的平台。</p>
           <p>
-            BBS-GO&nbsp;是一款基于Go语言研发的精美小巧的开源社区论坛系统。初期该项目仅用过学习和交流，开源之后越来越多的小伙伴儿开始喜欢和关注他，这也是我长期升级和维护的动力。
+            本人2022年毕业，在某厂任前端开发工程师，刚毕业就开始接受996的摧残，看着疲惫的同事们，我深刻地认识到，内卷是没有前途的，唯有【润】才能自我拯救。
+          </p>
+          <p>
+            计划2年内存够30万rmb，之后申请国外硕士（不排除水硕😂），希望能与志同道合的伙伴们一起努力💪。
           </p>
 
-          <p>
+          <!-- <p>
             为方便大家学习和交流我们在2019年8月使用&nbsp;BBS-GO&nbsp;搭建了&nbsp;<a
               href="https://mlog.club"
               target="_blank"
               >码农俱乐部</a
             >，欢迎大家前往交流。
-          </p>
+          </p> -->
 
           <h2>与我联系</h2>
           <p>
-            如有任何想法、意见、问题可前往&nbsp;<a
-              href="https://mlog.club"
+            如想交流经验，或有任何想法、意见、问题可前往&nbsp;
+            <a
               target="_blank"
-              >码农俱乐部</a
-            >&nbsp;交流。
+              href="https://qm.qq.com/cgi-bin/qm/qr?k=SVHxQmgG3sLS2CB37Njmwndt3XyQWywt&jump_from=webapi"
+            >
+              小润书-润润团qq群153361957
+            </a>
+            &nbsp;交流。
           </p>
-          <h2>项目贡献者</h2>
+          <!-- <h2>项目贡献者</h2>
           <p>
             <a
               href="https://github.com/mlogclub/bbs-go/graphs/contributors"
               target="_blank"
               >Contributors</a
             >
-          </p>
+          </p> -->
         </div>
       </div>
     </div>
